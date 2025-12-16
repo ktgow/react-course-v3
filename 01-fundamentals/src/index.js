e187e286196b1a9aefe156acb61bdf6f9d12353f
KDeus <deus@growf.com>
2025-12-16 02:20:29 +0000

    Trying fragments and createElement.

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-    return <h2>My First Component</h2>;
+    return (
+	<>
+	    <h2>My First Component</h2>
+	    <p>A paragraph.</p>
+	</>
+    );
 }
-//export default Greeting;
+// function Greeting() {
+//     return React.createElement('h2', {}, 'Hello, world!');
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
