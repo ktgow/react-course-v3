13ee90d6cebffbb67382c1305d51f904d60c8f8d
KD <deus@growf.com>
2025-12-15 19:26:22 -0800

    Changed 01 fundamentals to display Amazon's top 3 books (all hardcoded).

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Greeting() {
+import './index.css';
+
+const firstBook = {
+    img: './images/HowToDrawEverything.jpg',
+    title: 'How To Draw Everything',
+    author: 'Emma Greene',
+};
+const secondBook = {
+    img: './images/LearnALot.jpg',
+    title: 'Learn A Lot While You Sit On The Pot',
+    author: 'Jack Haynes',
+};
+const thirdBook = {
+    img: './images/FromCrookToCook.jpg',
+    title: 'From Crook to Cook',
+    author: 'Snoop Dogg',
+}
+
+function BookList() {
+    return <section className='booklist'>
+	       <Book
+		   author={firstBook.author}
+		   title={firstBook.title}
+		   img={firstBook.img}>
+		   First!
+	       </Book>
+	       <Book
+		   author={secondBook.author}
+		   title={secondBook.title}
+		   img={secondBook.img} />
+	       <Book
+		   author={thirdBook.author}
+		   title={thirdBook.title}
+		   img={thirdBook.img} />
+	   </section>;
+};
+
+function Book({img, title, author, children}) {
     return (
-	<>
-	    <h2>My First Component</h2>
-	    <p>A paragraph.</p>
-	</>
+	<article className='book'>
+	    <img src={img} alt={title} />
+	    <h2>{title}</h2>
+	    <h3>{author}</h3>
+	    {children}
+	</article>
     );
 }
-// function Greeting() {
-//     return React.createElement('h2', {}, 'Hello, world!');
-// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting />);
+root.render(<BookList />);
