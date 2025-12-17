b9e41c705233ca7bff26544ca8174ec9e0011b95
KD <deus@growf.com>
2025-12-17 15:45:11 -0800

    Updated title.

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -9,8 +9,8 @@ import './index.css';
 function BookList() {
     const getBook = (id) => books.find((book) => book.id === id);
     return (
-	<section>
-	    <h1>Top Selling Books</h1>
+	<>
+	    <h1 className='title'>Top Selling Books</h1>
 	    <div className='booklist'>
 		{books.map((book, index) =>
 		    <Book
@@ -21,7 +21,7 @@ function BookList() {
 		    />)
 		}
 	    </div>
-	</section>
+	</>
     );
 };
 
